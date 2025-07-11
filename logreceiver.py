f92f6013855d90add32f05a209821176b150c87f
Noah Lee <chaeuk83@gmail.com>
2025-07-11 15:09:00 +0900

    logreceiver: support print color depends on logtag

--- logreceiver.py
+++ logreceiver.py
@@ -7,7 +7,6 @@ import time
 import conf_parser
 
 BUF_SIZE = 1024
-
 condition=threading.Condition()
 
 def recv_data(sock):
@@ -28,6 +27,19 @@ def recv_data(sock):
 
     return ""
 
+def print_color(logstr):
+    if logstr.startswith(LOGTAG_0):
+        print("\033[96m", end='')
+        print(logstr)
+        print("\033[0m", end='')
+    elif logstr.startswith(LOGTAG_1):
+        print("\033[92m", end='')
+        print(logstr)
+        print("\033[0m", end='')
+    else:
+        print(logstr)
+
+
 def main(argc, argv):
     print("Trying to connect to server")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,13 +56,27 @@ def main(argc, argv):
 
     sock.send(mode.encode())
 
+    tempstr = ""
     while True:
         try:
             msg = recv_data(sock)
             if msg == "":
                 break
 
-            print(msg, end='')
+            logarr = msg.strip('\r').split('\n')
+            i = 0
+            for logline in logarr:
+                if len(logline):
+                    if tempstr != "":
+                        logline = tempstr + logline
+                        tempstr = ""
+
+                    if len(msg) == BUF_SIZE and i == len(logarr) -1:
+                        tempstr = logline
+                    else:
+                        print_color(logline)
+                i+=1
+            #print(msg, end='')
 
         except KeyboardInterrupt:
             print("Thread exit by KeyboardInterrupt")
@@ -62,9 +88,11 @@ def main(argc, argv):
 def get_config():
     config = conf_parser.get_config("LOGRECEIVER")
 
-    global DEFAULT_HOST, DEFAULT_PORT
+    global DEFAULT_HOST, DEFAULT_PORT, LOGTAG_0, LOGTAG_1
     DEFAULT_HOST = config["host_addr"]
     DEFAULT_PORT = int(config["host_port_num"])
+    LOGTAG_0 = config["logtag_0"]
+    LOGTAG_1 = config["logtag_1"]
 
 if __name__ == '__main__':
     get_config()
